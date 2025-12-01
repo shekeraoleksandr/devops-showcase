7bf9c91c7dac6c6c46b530cc89cd5fb9dc5cd142
shekeraoleksandr <oleksandrshekera@gmail.com>
2025-12-01 13:19:18 +0200

    Shipping Service: AWS migration

--- services/shippingservice/main.go
+++ services/shippingservice/main.go
@@ -20,7 +20,7 @@ import (
 	"os"
 	"time"
 
-	"cloud.google.com/go/profiler"
+	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -102,6 +102,7 @@ func main() {
 // server controls RPC service responses.
 type server struct {
 	pb.UnimplementedShippingServiceServer
+	healthpb.UnimplementedHealthServer
 }
 
 // Check is for health checking.
@@ -155,23 +156,13 @@ func initTracing() {
 }
 
 func initProfiling(service, version string) {
-	// TODO(ahmetb) this method is duplicated in other microservices using Go
-	// since they are not sharing packages.
-	for i := 1; i <= 3; i++ {
-		if err := profiler.Start(profiler.Config{
-			Service:        service,
-			ServiceVersion: version,
-			// ProjectID must be set if not running on GCP.
-			// ProjectID: "my-project",
-		}); err != nil {
-			log.Warnf("failed to start profiler: %+v", err)
-		} else {
-			log.Info("started Stackdriver profiler")
-			return
-		}
-		d := time.Second * 10 * time.Duration(i)
-		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
-		time.Sleep(d)
+	// Configure AWS X-Ray
+	err := xray.Configure(xray.Config{
+		ServiceVersion: version,
+	})
+	if err != nil {
+		log.Warnf("failed to configure AWS X-Ray: %+v", err)
+		return
 	}
-	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
+	log.Info("AWS X-Ray configured successfully")
 }
